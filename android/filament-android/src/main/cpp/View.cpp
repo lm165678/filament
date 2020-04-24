1e9ea55d66c15319fb89a1bf5e4f818accfd60f5
Pixelflinger <pixelflinger@gmail.com>
2020-04-23 20:39:07 -0700

    A new depth of field (DoF) post-process effect

--- android/filament-android/src/main/cpp/View.cpp
+++ android/filament-android/src/main/cpp/View.cpp
@@ -252,3 +252,10 @@ Java_com_google_android_filament_View_nSetBlendMode(JNIEnv *, jclass , jlong nat
     View* view = (View*) nativeView;
     view->setBlendMode((View::BlendMode)blendMode);
 }
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_google_android_filament_View_nSetDepthOfFieldOptions(JNIEnv *, jclass ,
+        jlong nativeView, jfloat focusDistance, jboolean enabled) {
+    View* view = (View*) nativeView;
+    view->setDepthOfFieldOptions({ .focusDistance = focusDistance, .enabled = (bool)enabled });
+}
